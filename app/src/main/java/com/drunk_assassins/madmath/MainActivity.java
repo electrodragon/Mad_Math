dddb7699bd39b96eedabcb82dc57c18284bf6bb2
electrodragon <attaullahkhanlodhi@gmail.com>
2019-12-22 21:03:24 +0500

    Complete Main Layout (LOGIX_WAY) !

--- app/src/main/java/com/drunk_assassins/madmath/MainActivity.java
+++ app/src/main/java/com/drunk_assassins/madmath/MainActivity.java
@@ -3,6 +3,7 @@ package com.drunk_assassins.madmath;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void playBtnClicked(View view) {
+    }
+
+    public void highScoreBtnClicked(View view) {
+    }
+
+    public void exitBtnClicked(View view) {
     }
 }
