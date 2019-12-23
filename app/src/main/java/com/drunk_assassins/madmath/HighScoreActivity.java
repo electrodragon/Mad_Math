9bd23148e38880762e01d839a4a0ba13fee40955
electrodragon <attaullahkhanlodhi@gmail.com>
2019-12-23 16:15:23 +0500

    Added onBackPressed Method in HighScoreActivity !

--- app/src/main/java/com/drunk_assassins/madmath/HighScoreActivity.java
+++ app/src/main/java/com/drunk_assassins/madmath/HighScoreActivity.java
@@ -2,6 +2,7 @@ package com.drunk_assassins.madmath;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class HighScoreActivity extends AppCompatActivity {
@@ -11,4 +12,11 @@ public class HighScoreActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_high_score);
     }
+
+    @Override
+    public void onBackPressed() {
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
+    }
+
 }
